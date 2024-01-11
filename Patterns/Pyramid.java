7b8f368c71bbb3d6b3ae8e7a30513f31ca54c964
BHARTI RANA <139580465+bharti2430@users.noreply.github.com>
2024-01-12 00:58:51 +0530

    Update Pyramid.java

--- Patterns/Pyramid.java
+++ Patterns/Pyramid.java
@@ -1,4 +1,6 @@
-public static void nStarTriangle(int n) {
+// method 1
+public static void nStarTriangle(int n) 
+{
      int star=1;
      for(int i=0;i<n;i++){
        for(int j=n-i;j>1;j--){
@@ -9,12 +11,23 @@ public static void nStarTriangle(int n) {
        }
        System.out.println();
        star+=2;
+     }
 }
 
-
-
-}
-
-
-
-    }
+// method 2
+for(int i=0;i<n;i++)
+  {
+      for(int j=0;j<(n-i-1);j++)
+      {
+          System.out.print(" ");
+      }
+      for(int j=0;j<(2*i+1);j++)
+      {
+          System.out.print("*");
+      }
+      /*for(int j=0;j<(n-i-1);j++)
+      {
+          System.out.print(" ");
+      }*/
+      System.out.println();
+  }
