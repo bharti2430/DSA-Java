50c7e28ee3c2a5b0f9d1e81ae4d533b64c721f89
BHARTI RANA <139580465+bharti2430@users.noreply.github.com>
2024-01-12 01:18:52 +0530

    Create ReversePyramid.java

---
+++ Patterns/ReversePyramid.java
@@ -0,0 +1,15 @@
+public static void nStarTriangle(int n) {
+        // Write your code here
+        for(int i=0;i<n;i++)
+        {
+            for(int j=1;j<=i;j++)
+            {
+                System.out.print(" ");
+            }
+            for(int j=0;j<(2*n-(2*i+1));j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
