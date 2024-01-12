c5b46f5008935180397e12bfb77fe36ff2bcbffe
BHARTI RANA <139580465+bharti2430@users.noreply.github.com>
2024-01-12 19:20:07 +0530

    Create BinaryTriangle.java

---
+++ Patterns/BinaryTriangle.java
@@ -0,0 +1,15 @@
+public static void nBinaryTriangle(int n) {
+        // Write your code here.
+        int binary=1;
+        for(int i=1;i<=n;i++)
+        {
+            if(i%2==0) binary=0;
+            else binary=1;
+            for(int j=0;j<i;j++)
+            {
+                System.out.print(binary+" ");
+                binary=1-binary;
+            }
+            System.out.println();
+        }
+    }
