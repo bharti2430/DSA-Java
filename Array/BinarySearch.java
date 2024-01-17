f56eebe633a299d5dee3d33d3e51bc73ea304469
BHARTI RANA <139580465+bharti2430@users.noreply.github.com>
2024-01-17 14:43:20 +0530

    Create BinarySearch.java

---
+++ Array/BinarySearch.java
@@ -0,0 +1,16 @@
+public int search(int[] arr, int target) {
+        int n=arr.length;
+        int start=0,end=n-1;
+        
+        while(start<=end)
+        {
+            int mid=(start+end)/2;
+            if(arr[mid]==target)
+                return mid;
+            if(arr[mid]>target)
+                end=mid-1;
+            else
+                start=mid+1;
+        }
+        return -1;
+    }
