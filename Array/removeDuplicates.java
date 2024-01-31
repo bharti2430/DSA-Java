91dc1879dd3ea6585b7fd58e1f8a06dcdb2dd8e2
BHARTI RANA <139580465+bharti2430@users.noreply.github.com>
2024-01-31 16:57:14 +0530

    Update removeDuplicates.java

--- Array/removeDuplicates.java
+++ Array/removeDuplicates.java
@@ -10,3 +10,9 @@ public int removeDuplicates(int[] nums) {
         }
         return j;
     }
+
+//output
+Input: nums = [1,1,2]
+Output: 2, nums = [1,2,_]
+Explanation: Your function should return k = 2, with the first two elements of nums being 1 and 2 respectively.
+It does not matter what you leave beyond the returned k (hence they are underscores).
