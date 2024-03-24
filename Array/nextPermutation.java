c3819a09a4048020314cdfd4473c5741aa7c2c3a
BHARTI RANA <139580465+bharti2430@users.noreply.github.com>
2024-03-24 08:55:43 +0530

    Update nextPermutation.java

--- Array/nextPermutation.java
+++ Array/nextPermutation.java
@@ -32,3 +32,43 @@ Input:
 A = [3, 1, 2]
 Output:
 3 2 1
+
+        public void nextPermutation(int[] nums) {
+        int index=-1;
+        final int n=nums.length;
+        for(int i=n-2;i>=0;i--)
+        {
+            if(nums[i]<nums[i+1])
+            {
+                index=i;
+                break;
+            }
+        }
+        if(index>=0)
+        {
+            for(int i=n-1;i>=index;i--)
+            {
+                if(nums[i]>nums[index])
+                {
+                    swap(nums,i,index);
+                    break;
+                }
+            }
+        }
+        reverse(nums,index+1,n-1);
+    }
+    private void swap(int [] nums,int a,int b)
+    {
+        final int temp=nums[a];
+        nums[a]=nums[b];
+        nums[b]=temp;
+    }
+    private void reverse(int[] nums,int start,int end)
+    {
+        while(start<end)
+        {
+            swap(nums,start,end);
+            start++;
+            end--;
+        }
+    }
