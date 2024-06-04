dadbef31c65672af62ff5f00051bf95216d8cc21
BHARTI RANA <139580465+bharti2430@users.noreply.github.com>
2024-06-04 12:57:20 +0530

    Update PalindromicLL.java

--- Linked List/PalindromicLL.java
+++ Linked List/PalindromicLL.java
@@ -18,3 +18,30 @@ Input: head = [1,2,2,1]
 Output: true
 
 //OPTIMAL
+public boolean isPalindrome(ListNode head) {
+        ListNode slow=head,fast=head;
+        while(fast.next!=null && fast.next.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        ListNode newHead=reverse(slow.next);
+        ListNode first=head,second=newHead;
+        while(first!=null && second!=null){
+            if(first.val!=second.val) return false;
+            first=first.next;
+            second=second.next;
+        }
+        reverse(newHead);
+        return true;
+    }
+    public ListNode reverse(ListNode temp)
+    {
+        ListNode prev=null,current=temp,front;
+        while(current!=null){
+            front=current.next;
+            current.next=prev;
+            prev=current;
+            current=front;
+        }
+        return prev;
+    }
