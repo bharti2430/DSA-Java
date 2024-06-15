31f87534288e90d8799eed66c83e9a037a1d61c2
BHARTI RANA <139580465+bharti2430@users.noreply.github.com>
2024-06-15 16:25:08 +0530

    Create NStacksInArray.java

---
+++ Stack/NStacksInArray.java
@@ -0,0 +1,65 @@
+public class NStack {
+    int n,s,freespot;
+    int top[];
+    int next[];
+    int arr[];
+    public NStack(int N, int S) {
+        n=N;
+        s=S;
+        freespot=0;
+        top=new int[n];
+        next=new int[s];
+        arr=new int[s];
+        for(int i=0;i<n;i++){
+            top[i]=-1;
+        }
+        for(int i=0;i<s;i++){
+            next[i]=i+1;
+        }
+        next[s-1]=-1;
+    }
+
+    // Pushes 'X' into the Mth stack. Returns true if it gets pushed into the stack, and false otherwise.
+    public boolean push(int x, int m) {
+        if(freespot==-1) return false;
+        int index=freespot;
+        freespot=next[index];
+        arr[index]=x;
+        next[index]=top[m-1];
+        top[m-1]=index;
+        return true;
+    }
+
+    // Pops top element from Mth Stack. Returns -1 if the stack is empty, otherwise returns the popped element.
+    public int pop(int m) {
+        if(top[m-1]==-1) return -1;
+        int index=top[m-1];
+        top[m-1]=next[index];
+        next[index]=freespot;
+        freespot=index;
+        return arr[index];
+    }
+}
+
+Sample Input 1 :
+3 6 5
+1 10 1
+1 20 1
+1 30 2
+2 1
+2 2
+Sample Output 1 :
+True 
+True
+True
+20
+30
+Explanation 1 :
+For the given input, we have the number of stacks, N = 3, size of the array, S = 6 and number of queries, Q = 5.
+Operations performed on the stack are as follows:
+push(10, 1): Push element ‘10’ into the 1st stack. This returns true.
+push(20, 1): Push element ‘20’ into the 1st stack. This returns true.
+push(30, 2): Push element ‘30’ into the 2nd stack. This returns true.
+pop(1): Pop the top element from the 1st stack. This returns 20.
+pop(2): Pop the top element from the 2nd stack. This returns 30.
+
