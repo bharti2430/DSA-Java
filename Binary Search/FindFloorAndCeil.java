20dc049aead4cfc5d85044b6deb15e478bc1c8a6
BHARTI RANA <139580465+bharti2430@users.noreply.github.com>
2024-06-17 13:45:33 +0530

    Create FindFloorAndCeil.java

---
+++ Binary Search/FindFloorAndCeil.java
@@ -0,0 +1,41 @@
+Pair getFloorAndCeil(int[] arr, int n, int x) {
+        int floor=getFloor(arr,n,x);
+        int ceil=getCeil(arr,n,x);
+        Pair p=new Pair(floor,ceil);
+        return p;
+    }
+    private int getFloor(int[] arr,int n,int x){
+        int low = 0, high = n - 1;
+        int floor = -1;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (arr[mid] <= x) {
+                floor = arr[mid];
+                high = mid - 1;
+            } else {
+                low = mid + 1; 
+            }
+        }
+        return floor;
+    }
+    private int getCeil(int[] arr,int n,int x){
+        int low = 0, high = n - 1;
+        int ceil = -1;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (arr[mid] >= x) {
+                ceil = arr[mid];
+                high = mid - 1;
+            } else {
+                low = mid + 1; 
+            }
+        }
+        return ceil;
+    }
+
+Input:
+N = 8, X = 7
+Arr[] = {5, 6, 8, 9, 6, 5, 5, 6}
+Output: 6 8
