b918049e7cf72b84ef1589abf59a5d8b6c6652c7
BHARTI RANA <139580465+bharti2430@users.noreply.github.com>
2024-06-18 20:06:56 +0530

    Update SearchInRotatedSortedArray.java

--- Binary Search/SearchInRotatedSortedArray.java
+++ Binary Search/SearchInRotatedSortedArray.java
@@ -1,3 +1,4 @@
+// type 1
 public int search(int[] nums, int target) {
         int low=0,high=nums.length-1;
         while(low<=high){
@@ -24,3 +25,40 @@ Input: nums = [4,5,6,7,0,1,2], target = 0
 Output: 4
 Input: nums = [4,5,6,7,0,1,2], target = 3
 Output: -1
+
+//type 2
+public boolean search(int[] nums, int target) {
+        int low=0,high=nums.length-1;
+        while(low<=high){
+            int mid=(low+high)/2;
+            if(nums[mid]==target){
+                return true;
+            }
+            if(nums[low]==nums[mid] && nums[mid]==nums[high]){
+                low=low+1;
+                high=high-1;
+                continue;
+            }
+            //left sorted
+            if(nums[low]<=nums[mid]){
+                if(nums[low]<=target && target<=nums[mid])
+                    high=mid-1;
+                else
+                    low=mid+1;
+            }
+            // right sorted
+            else
+            {
+                if(target>=nums[mid] && nums[high]>=target)
+                    low=mid+1;
+                else
+                    high=mid-1;
+            }
+        }
+        return false;
+    }
+
+Input: nums = [2,5,6,0,0,1,2], target = 0
+Output: true
+Input: nums = [2,5,6,0,0,1,2], target = 3
+Output: false
