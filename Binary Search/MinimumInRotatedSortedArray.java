753ae73b70f3f7d26dcd991ed8d54c2be2ce8388
BHARTI RANA <139580465+bharti2430@users.noreply.github.com>
2024-06-18 20:11:58 +0530

    Create MinimumInRotatedSortedArray.java

---
+++ Binary Search/MinimumInRotatedSortedArray.java
@@ -0,0 +1,20 @@
+public int findMin(int[] nums) {
+        int min=Integer.MAX_VALUE;
+        int low=0,high=nums.length-1;
+        while(low<=high){
+            int mid=(low+high)/2;
+            //left sorted
+            if(nums[low]<=nums[mid]){
+                min=Math.min(min,nums[low]);
+                low=mid+1;
+            }
+            else{
+                min=Math.min(min,nums[mid]);
+                high=mid-1;
+            }
+        }
+        return min;
+    }
+
+Input: nums = [3,4,5,1,2]
+Output: 1
