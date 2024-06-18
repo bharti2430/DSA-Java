3b37ae05d52f59723ad0d51a8de3c2e7e5a3f42d
BHARTI RANA <139580465+bharti2430@users.noreply.github.com>
2024-06-18 20:31:25 +0530

    Create SingleElement.java

---
+++ Binary Search/SingleElement.java
@@ -0,0 +1,32 @@
+// all element appears twice except one
+//brute 1
+public int singleNonDuplicate(int[] nums) {
+        int xor=0;
+        for(int i=0;i<nums.length;i++){
+            xor=xor^nums[i];
+        }
+        return xor;
+    }
+
+Input: nums = [1,1,2,3,3,4,4,8,8]
+Output: 2
+
+//brute 2
+public int singleNonDuplicate(int[] nums) {
+        if(nums.length==1) return nums[0];
+        for(int i=0;i<nums.length;i++){
+            if(i==0){
+                if(nums[0]!=nums[1])
+                    return nums[0];
+            }
+            else if(i==nums.length-1){
+                if(nums[nums.length-1]!=nums[nums.length-2])
+                    return nums[nums.length-1];
+            }
+            else {
+                    if(nums[i]!=nums[i-1] && nums[i]!=nums[i+1])
+                        return nums[i];
+            }
+        }
+        return -1;
+    }
