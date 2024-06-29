bfa944edba891ba9732f7e32ddccaf594fd43f11
BHARTI RANA <139580465+bharti2430@users.noreply.github.com>
2024-06-29 13:21:23 +0530

    Update SingleElement.java

--- Binary Search/SingleElement.java
+++ Binary Search/SingleElement.java
@@ -30,3 +30,22 @@ public int singleNonDuplicate(int[] nums) {
         }
         return -1;
     }
+
+//optimal
+public int singleNonDuplicate(int[] nums) {
+        int n=nums.length;
+        if(n==1) return nums[0];
+        if(nums[0]!=nums[1]) return nums[0];
+        if(nums[n-1]!=nums[n-2]) return nums[n-1];
+        int low=1,high=n-2;
+        while(low<=high){
+            int mid=(low+high)/2;
+            if(nums[mid]!=nums[mid-1] && nums[mid]!=nums[mid+1])
+                return nums[mid];
+            if((mid%2!=0 && nums[mid]==nums[mid-1]) || (mid%2==0 && nums[mid]==nums[mid+1]))
+                low=mid+1;
+            else
+                high=mid-1;
+        }
+        return -1;
+    }
