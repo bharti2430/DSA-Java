e8a4a2e5daa29f29d1c3722586162ff20e85cab0
BHARTI RANA <139580465+bharti2430@users.noreply.github.com>
2024-07-08 09:51:47 +0530

    Update CombinationSum-III.java

--- Recursion/CombinationSum-III.java
+++ Recursion/CombinationSum-III.java
@@ -29,3 +29,26 @@ Input: k = 3, n = 7
 Output: [[1,2,4]]
 Input: k = 3, n = 9
 Output: [[1,2,6],[1,3,5],[2,3,4]]
+
+public List<List<Integer>> combinationSum3(int k, int n) {
+        List<List<Integer>> ans=new ArrayList<>();
+        helper(1,k,n,new ArrayList<>(),ans);
+        return ans;
+    }
+    private void helper(int start,int k,int n,List<Integer> list,List<List<Integer>> ans){
+        if(list.size()==k){
+            if(n==0){
+                ans.add(new ArrayList<>(list));
+                return;
+            }
+            return;
+        }
+        for(int i=start;i<=9;i++){
+            if(i>n){
+                break;
+            }
+            list.add(i);
+            helper(i+1,k,n-i,list,ans);
+            list.remove(list.size()-1);
+        }
+    }
