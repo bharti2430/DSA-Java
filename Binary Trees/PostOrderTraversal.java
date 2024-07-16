30414a3f8ef3a6db8339d3c516ae09136acbf558
BHARTI RANA <139580465+bharti2430@users.noreply.github.com>
2024-07-16 14:11:09 +0530

    Update PostOrderTraversal.java

--- Binary Trees/PostOrderTraversal.java
+++ Binary Trees/PostOrderTraversal.java
@@ -16,3 +16,29 @@ Output: [3,2,1]
 
 Input: root = []
 Output: []
+
+// Iterative approach
+public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> result=new ArrayList<>();
+        if(root==null){
+            return result;
+        }
+        Stack<TreeNode> st1=new Stack<>();
+        Stack<TreeNode> st2=new Stack<>();
+        st1.push(root);
+        while(!st1.isEmpty()){
+            TreeNode node=st1.pop();
+            st2.push(node);
+            if(node.left!=null){
+                st1.push(node.left);
+            }
+            if(node.right!=null){
+                st1.push(node.right);
+            }
+        }
+        while(!st2.isEmpty()){
+            TreeNode node=st2.pop();
+            result.add(node.val);
+        }
+        return result;
+    }
