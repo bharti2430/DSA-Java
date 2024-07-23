72bde72855ebec3721960b0448b2e895117c6d1e
BHARTI RANA <139580465+bharti2430@users.noreply.github.com>
2024-07-23 10:01:06 +0530

    Update RootToLeafPath.java

--- Binary Trees/RootToLeafPath.java
+++ Binary Trees/RootToLeafPath.java
@@ -35,3 +35,32 @@ Output:
 10 20 40 
 10 20 60 
 10 30 
+
+public List<String> binaryTreePaths(TreeNode root) {
+        List<String> ans=new ArrayList<>();
+        if(root==null) return ans;
+        helper(root,new StringBuilder(),ans);
+        return ans;
+    }
+    private void helper(TreeNode root, StringBuilder sb, List<String> result) {
+        int len=sb.length(); // store current length 
+        sb.append(root.val);
+        if (root.left == null && root.right == null) {
+            result.add(sb.toString());
+        } else {
+            sb.append("->");
+            if (root.left != null) {
+                helper(root.left, sb, result);
+            }
+            if (root.right != null) {
+                helper(root.right, sb, result);
+            }
+        }
+        sb.setLength(len);
+    }
+Input: root = [1,2,3,null,5]
+Output: ["1->2->5","1->3"]
+
+Input: root = [1]
+Output: ["1"]
+
