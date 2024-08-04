81d0757bd5ef7d84568b8d16118858a9c9bd0d71
BHARTI RANA <139580465+bharti2430@users.noreply.github.com>
2024-08-04 07:53:53 +0530

    Update LCAInBST.java

--- Binary Search Tree/LCAInBST.java
+++ Binary Search Tree/LCAInBST.java
@@ -1,1 +1,16 @@
+public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if(root==null) return null;
+        int data=root.val;
+        if(data<p.val && data<q.val)
+            return lowestCommonAncestor(root.right,p,q);
+        if(data>p.val && data>q.val)
+            return lowestCommonAncestor(root.left,p,q);
+        return root;
+    }
 
+Input: root = [6,2,8,0,4,7,9,null,null,3,5], p = 2, q = 8
+Output: 6
+Explanation: The LCA of nodes 2 and 8 is 6.
+
+Input: root = [6,2,8,0,4,7,9,null,null,3,5], p = 2, q = 4
+Output: 2
