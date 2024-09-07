c90306c6cb4d407d65459fa9d15f250f2df43f59
BHARTI RANA <139580465+bharti2430@users.noreply.github.com>
2024-09-07 07:46:56 +0530

    Update ValidAnagram.java

--- Strings/ValidAnagram.java
+++ Strings/ValidAnagram.java
@@ -23,3 +23,20 @@ Input:a = geeksforgeeks, b = forgeeksgeeks
 Output: YES
 Explanation: Both the string have same characters with
         same frequency. So, both are anagrams.
+
+// 2
+public boolean isAnagram(String s, String t) {
+
+        if(s.length()!=t.length())
+            return false;
+        int[] alphabet = new int[26];
+        for (int i = 0; i < s.length(); i++) 
+            alphabet[s.charAt(i) - 'a']++;
+        for (int i = 0; i < t.length(); i++) 
+            alphabet[t.charAt(i) - 'a']--;
+        for (int i : alphabet) 
+            if (i != 0) 
+                return false;
+        return true;
+    }
+
